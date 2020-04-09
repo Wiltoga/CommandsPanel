683b216df940b63a0d974d9084572a77acd5acc9
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-04-09 10:46:21 +0200

    added icon and other testing stuff

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandsPannel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window_MouseLeave(null, null);
+            for (int i = 0; i < 10; i++)
+                icons.Children.Add(new Button { Content = "uijytrhgevfcds", Height = 100, Width = 100 });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Topmost = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Topmost = false;
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+                for (int i = 0; i < e.Delta; i++)
+                    (sender as ScrollViewer).LineLeft();
+            else if (e.Delta < 0)
+                for (int i = 0; i < -e.Delta; i++)
+                    (sender as ScrollViewer).LineRight();
+        }
+
+        private void Window_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Top -= 200;
+            Height = 450;
+            Width = 800;
+            pannel.Visibility = Visibility.Visible;
+        }
+
+        private void Window_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Top += 200;
+            Height = 250;
+            Width = 20;
+            pannel.Visibility = Visibility.Hidden;
+        }
+    }
+}
